5be3b519cd7e394093e8451d324362d41003f9de
Melina1996 <melina.schlachter@gmx.de>
2024-02-26 11:06:33 +0100

    added header change depending on displayed site

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import Sidebar from "./Components/Sidebar";
 import Personal from "./Components/Personal";
 import Header from "./Components/Header";
 
 function App() {
+  const [currentStep, setStep] = useState(1);
 
-  const[currentStep,setStep]=useState(1)
+  let headerInfo = [{header: "Personal info", subtext: "Please provide your name, email address, and phone number."},{header: "Select your plan", subtext: "You have the option of monthly or yearly billing"},{header: "Pick add-ons", subtext: "Add-ons help enhance your gaming experience."},{header: "Finishing up", subtext: "Double-check everything looks OK before confirming."},{header:"",subtext:""}];
 
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-[#F0F4FDff]">
@@ -14,11 +15,12 @@ function App() {
           <Sidebar currentStep={currentStep} />
 
           <div className="w-[60%] h-[90%] flex flex-col justify-start items-start">
-            <Header />
+
+            <Header header={currentStep == 1 ? headerInfo[0].header : currentStep == 2 ? headerInfo[1].header : currentStep == 3 ? headerInfo[2].header : currentStep == 4? headerInfo[3].header : currentStep == 5 ? headerInfo[4].header : ""} subtext={currentStep == 1 ? headerInfo[0].subtext : currentStep == 2 ? headerInfo[1].subtext : currentStep == 3 ? headerInfo[2].subtext : currentStep == 4? headerInfo[3].subtext : currentStep == 5 ? headerInfo[4].subtext : ""}/>
 
             {/* {currentStep == 0 ?  */}
 
-              <Personal />
+            <Personal />
 
             {/* //   : currentStep == 1 ?
 
@@ -26,24 +28,27 @@ function App() {
 
             // } */}
 
-            
-
             <div className="h-[10%] w-[100%] flex justify-center items-center">
-
               <div className="w-[50%] flex justify-start items-center">
-
-                <button onClick={()=> currentStep > 0 ? setStep(currentStep-1) : ""} className="text-[#012A5Cff] font-semibold">
+                <button
+                  onClick={() =>
+                    currentStep > 0 ? setStep(currentStep - 1) : ""
+                  }
+                  className="text-[#012A5Cff] font-semibold"
+                >
                   Go back
                 </button>
-
               </div>
 
               <div className="w-[50%] flex justify-end items-center">
-
-                <button onClick={()=> currentStep <5 ? setStep(currentStep+1) : ""} className="bg-[#012A5Cff] rounded-lg text-white p-3">
+                <button
+                  onClick={() =>
+                    currentStep < 5 ? setStep(currentStep + 1) : ""
+                  }
+                  className="bg-[#012A5Cff] rounded-lg text-white p-3"
+                >
                   Next step
                 </button>
-
               </div>
             </div>
           </div>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function Header() {
+export default function Header(props) {
   return (
 
     <div className='w-[100%] h-[20%]'>
 
-        <h1 className='text-[#012A5Bff] text-[35px]'>Personal info</h1>
-        <p className='text-[#C8C8C8ff] text-[15px]'>Please provide your name, email address, and phone number.</p>
+        <h1 className='text-[#012A5Bff] text-[35px]'>{props.header}</h1>
+        <p className='text-[#C8C8C8ff] text-[15px]'>{props.subtext}</p>
 
     </div>
   )
